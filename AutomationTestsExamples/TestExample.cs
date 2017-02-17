6a4b69e1aa66c0c1dddf8bc937a49670f8b582ed
Žymantas <z.kuodys@gmail.com>
2017-02-17 12:50:05 +0200

    Automation tests PageObject pattern

---
+++ AutomationTestsExamples/TestExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomationTestsExamples
+{
+    [TestClass]
+    public class TestExample
+    {
+        public string searchKeyword = "asd";
+
+        private IWebDriver _driver;
+        private TestPage _testPage;
+
+        [TestInitialize]
+        public void BeforeTests()
+        {
+            _driver = new ChromeDriver();
+            _testPage = new TestPage(_driver);
+
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            _driver.Navigate().GoToUrl("http://");
+            _testPage.ClickItem();
+            _testPage.SendKeYS("");
+            _testPage.ClickItem();
+        }
+
+        [TestMethod]
+        public void TestCreateNewExpense()
+        {
+            _testPage.SendKeYS(searchKeyword);
+            _testPage.assert(searchKeyword);
+            _testPage.hoveronaccount();
+            _testPage.ClickItem();
+        }
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            _driver.Quit();
+        }
+    }
+}
